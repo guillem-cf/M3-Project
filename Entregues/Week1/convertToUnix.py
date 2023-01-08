c0cea947df1da16cbefe9d37d61e55b2a7a19064
johnnynunez <johnnynuca14@gmail.com>
2023-01-08 22:28:32 +0100

    entregues

---
+++ Entregues/Week1/convertToUnix.py
@@ -0,0 +1,21 @@
+
+def convertToUnix (source, destination):
+    """
+    convert dos linefeeds (crlf) to unix (lf)
+    usage: dos2unix.py 
+    """
+
+
+    content = ''
+    outsize = 0
+    with open(source, 'rb') as infile:
+        content = infile.read()
+    with open(destination, 'wb') as output:
+        for line in content.splitlines():
+            outsize += len(line) + 1
+            output.write(line + str.encode('\n'))
+
+    print("Done. Saved %s bytes." % (len(content)-outsize))
+
+convertToUnix("MIT_split/test_labels.dat","MIT_split/test_labels_unix.dat")
+
