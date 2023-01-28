99bd7e04c5ba431e7e4babb701f2d8461aa649b4
guillem-cf <1659365@uab.cat>
2023-01-28 11:29:06 +0100

    .

--- Week4/3.dataugmentation.py
+++ Week4/3.dataugmentation.py
@@ -3,8 +3,7 @@ import tensorflow as tensorflow
 import wandb
 from tensorflow.keras.applications.densenet import DenseNet121
 from tensorflow.keras.applications.densenet import preprocess_input
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Flatten, Dense, GlobalAveragePooling2D
 from tensorflow.keras.models import Model
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.utils import plot_model
@@ -78,7 +77,7 @@ args = parser.parse_args()
 
 sweep_config = {
         'method': 'grid',
-        'name': 'sweep',
+        'name': 'Task3',
         'metric': {'goal': 'maximize', 'name': 'val_accuracy'},
         'parameters': 
         {
@@ -111,9 +110,9 @@ def train():
     wandb.init(project=args.experiment_name)
 
     datagen = ImageDataGenerator(
-        featurewise_center=False,
+        featurewise_center=True,
         samplewise_center=False,
-        featurewise_std_normalization=False,
+        featurewise_std_normalization=True,
         samplewise_std_normalization=False,
         preprocessing_function=preprocess_input,
         rotation_range=wandb.config.data_augmentation_R,
@@ -155,25 +154,26 @@ def train():
     # base_model.summary()
     # plot_model(base_model, to_file='modelDenseNet121.png', show_shapes=True, show_layer_names=True)
 
-    x = Flatten()(base_model.output)
-    x = Dense(512, activation="relu")(x)
-    x = Dense(256, activation="relu")(x)
-    output = Dense(8, activation="softmax", name="predictions")(x)
+    # We choose the model 1
+    x = base_model.get_layer('pool4_conv').output  # -1 block + -1 transient
 
-    model = Model(inputs=base_model.input, outputs=output)
-    model.summary()
-    plot_model(model, to_file="modelDenseNet121c.png", show_shapes=True, show_layer_names=True)
+    x = GlobalAveragePooling2D()(x)
+    # x = Dense(1024, activation='relu')(x)
+    x = Dense(8, activation='softmax', name='predictionsProf')(x)
+
+    model = Model(inputs=base_model.input, outputs=x)
+    # model.summary()
+    # plot_model(model, to_file="modelDenseNet121c.png", show_shapes=True, show_layer_names=True)
 
     # defining the early stop criteria
-    es = EarlyStopping(monitor="val_loss", mode="min", verbose=1, patience=20)
+    es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=20)
     # saving the best model based on val_loss
-    mc = ModelCheckpoint(
-        "./checkpoint/best_" + wandb.config.experiment_name + "_model_checkpoint" + ".h5",
-        monitor="val_loss",
-        mode="min",
-        save_best_only=True,
-    )
-    reduce_lr = ReduceLROnPlateau(monitor="val_loss", factor=0.2, patience=10, min_lr=1e-6)
+    mc1 = ModelCheckpoint('./checkpoint/best_' + args.experiment_name + '_model_checkpoint' + '.h5',
+                          monitor='val_loss', mode='min', save_best_only=True)
+    mc2 = ModelCheckpoint('./checkpoint/best_' + args.experiment_name + '_model_checkpoint' + '.h5',
+                          monitor='val_accuracy', mode='max', save_best_only=True)
+    reduce_lr = ReduceLROnPlateau(
+        monitor='val_loss', factor=0.2, patience=10, min_lr=1e-6)
 
     model.compile(loss="categorical_crossentropy", optimizer="adadelta", metrics=["accuracy"])
 
@@ -185,7 +185,7 @@ def train():
         epochs=wandb.config.EPOCHS,
         validation_data=validation_generator,
         validation_steps=(int(wandb.config.VALIDATION_SAMPLES // wandb.config.BATCH_SIZE) + 1),
-        callbacks=[WandbCallback()],
+        callbacks=[WandbCallback(), mc1, mc2],
     )
     # callbacks=[es, mc, mc_2, reduce_lr, WandbCallback()])
     # https://www.tensorflow.org/api_docs/python/tensorflow/keras/callbacks/ReduceLROnPlateau
@@ -239,76 +239,81 @@ def train():
     # number_of_epoch=2
     # validation_samples=807
     # """
+    """
+    parser = argparse.ArgumentParser(
+        description="MIT", formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument("--DATASET_DIR", type=str, help="Dataset path", default="./MIT_split")
+    parser.add_argument(
+        "--MODEL_FNAME", type=str, default="./model/full_image/mlp", help="Model path"
+    )
+    parser.add_argument(
+        "--WEIGHTS_FNAME",
+        type=str,
+        default="./weights/full_image/mlp",
+        help="Weights path",
+    )
+    # parser.add_argument("--PATCH_SIZE", type=int, help="Indicate Patch Size", default=64)
+    parser.add_argument("--BATCH_SIZE", type=int, help="Indicate Batch Size", default=32)
+    parser.add_argument("--EPOCHS", type=int, help="Indicate Epochs", default=20)
+    parser.add_argument("--LEARNING_RATE", type=float, help="Indicate Learning Rate", default=0.001)
+    parser.add_argument("--MOMENTUM", type=float, help="Indicate Momentum", default=0.9)
+    parser.add_argument("--DROPOUT", type=float, help="Indicate Dropout", default=0)
+    parser.add_argument("--WEIGHT_DECAY", type=float, help="Indicate Weight Decay", default=0.0001)
+    parser.add_argument("--OPTIMIZER", type=str, help="Indicate Optimizer", default="sgd")
+    parser.add_argument(
+        "--LOSS", type=str, help="Indicate Loss", default="categorical_crossentropy"
+    )
+    parser.add_argument("--IMG_WIDTH", type=int, help="Indicate Image Size", default=224)
+    parser.add_argument("--IMG_HEIGHT", type=int, help="Indicate Image Size", default=224)
+    # parser.add_argument("--MODEL", type=int, help="Indicate the model to use", default=1)
+    parser.add_argument("--experiment_name", type=str, help="Experiment name", default="baseline")
+    parser.add_argument(
+        "--VALIDATION_SAMPLES",
+        type=int,
+        help="Number of validation samples",
+        default=807,
+    )
+    parser.add_argument("--horizontal_flip", type=bool, help="Horizontal Flip", default=False)
+    parser.add_argument("--vertical_flip", type=bool, help="Vertical Flip", default=False)
+    parser.add_argument("--rotation", type=int, help="Rotation", default=0)
+    parser.add_argument("--width_shift", type=float, help="Width Shift", default=0.0)
+    parser.add_argument("--height_shift", type=float, help="Height Shift", default=0.0)
+    parser.add_argument("--shear_range", type=float, help="Shear Range", default=0.0)
+    parser.add_argument("--zoom_range", type=float, help="Zoom Range", default=0.0)
 
-    # parser = argparse.ArgumentParser(
-    #     description="MIT", formatter_class=argparse.ArgumentDefaultsHelpFormatter
-    # )
-    # parser.add_argument("--DATASET_DIR", type=str, help="Dataset path", default="./MIT_split")
-    # parser.add_argument(
-    #     "--MODEL_FNAME", type=str, default="./model/full_image/mlp", help="Model path"
-    # )
-    # parser.add_argument(
-    #     "--WEIGHTS_FNAME",
-    #     type=str,
-    #     default="./weights/full_image/mlp",
-    #     help="Weights path",
-    # )
-    # # parser.add_argument("--PATCH_SIZE", type=int, help="Indicate Patch Size", default=64)
-    # parser.add_argument("--BATCH_SIZE", type=int, help="Indicate Batch Size", default=32)
-    # parser.add_argument("--EPOCHS", type=int, help="Indicate Epochs", default=20)
-    # parser.add_argument("--LEARNING_RATE", type=float, help="Indicate Learning Rate", default=0.001)
-    # parser.add_argument("--MOMENTUM", type=float, help="Indicate Momentum", default=0.9)
-    # parser.add_argument("--DROPOUT", type=float, help="Indicate Dropout", default=0)
-    # parser.add_argument("--WEIGHT_DECAY", type=float, help="Indicate Weight Decay", default=0.0001)
-    # parser.add_argument("--OPTIMIZER", type=str, help="Indicate Optimizer", default="sgd")
-    # parser.add_argument(
-    #     "--LOSS", type=str, help="Indicate Loss", default="categorical_crossentropy"
-    # )
-    # parser.add_argument("--IMG_WIDTH", type=int, help="Indicate Image Size", default=224)
-    # parser.add_argument("--IMG_HEIGHT", type=int, help="Indicate Image Size", default=224)
-    # # parser.add_argument("--MODEL", type=int, help="Indicate the model to use", default=1)
-    # parser.add_argument("--experiment_name", type=str, help="Experiment name", default="baseline")
-    # parser.add_argument(
-    #     "--VALIDATION_SAMPLES",
-    #     type=int,
-    #     help="Number of validation samples",
-    #     default=807,
-    # )
-    # parser.add_argument("--horizontal_flip", type=bool, help="Horizontal Flip", default=False)
-    # parser.add_argument("--vertical_flip", type=bool, help="Vertical Flip", default=False)
-    # parser.add_argument("--rotation", type=int, help="Rotation", default=0)
-    # parser.add_argument("--width_shift", type=float, help="Width Shift", default=0.0)
-    # parser.add_argument("--height_shift", type=float, help="Height Shift", default=0.0)
-    # parser.add_argument("--shear_range", type=float, help="Shear Range", default=0.0)
-    # parser.add_argument("--zoom_range", type=float, help="Zoom Range", default=0.0)
-
-    # args = parser.parse_args()
-
-    # config = dict(
-    #     model_name=args.experiment_name,
-    #     LEARNING_RATE=args.LEARNING_RATE,
-    #     momentum=args.MOMENTUM,
-    #     architecture="MLP",
-    #     DATASET_DIR=args.DATASET_DIR,
-    #     OPTIMIZER=args.OPTIMIZER,  # sgd, adam, rmsprop
-    #     LOSS=args.LOSS,
-    #     IMG_WIDTH=args.IMG_WIDTH,
-    #     IMG_HEIGHT=args.IMG_HEIGHT,
-    #     BATCH_SIZE=args.BATCH_SIZE,
-    #     EPOCHS=args.EPOCHS,
-    #     VALIDATION_SAMPLES=args.VALIDATION_SAMPLES,
-    #     WEIGHT_DECAY=args.WEIGHT_DECAY,
-    #     DROPOUT=args.DROPOUT,
-    #     data_augmentation_HF = args.horizontal_flip,
-    #     data_augmentation_VF = args.vertical_flip,
-    #     data_augmentation_R = args.rotation,
-    #     data_augmentation_Z = args.zoom,
-    #     data_augmentation_W = args.width_shift,
-    #     data_augmentation_H = args.height_shift,
-    #     data_augmentation_S = args.shear
-    #     # model = args.MODEL,
-    # )
+    args = parser.parse_args()
 
 
+    sweep_config = {
+            'method': 'grid',
+            'name': 'Task3',
+            'metric': {'goal': 'maximize', 'name': 'val_accuracy'},
+            'parameters': 
+            {
+                'experiment_name': {'value': args.experiment_name},
+                'MODEL_FNAME':     {'value': args.MODEL_FNAME},
+                'DATASET_DIR':     {'value': args.DATASET_DIR},
+                'LEARNING_RATE':   {'value': args.LEARNING_RATE},
+                'EPOCHS':          {'value': args.EPOCHS},
+                'BATCH_SIZE':      {'value': args.BATCH_SIZE},
+                'OPTIMIZER':       {'value': args.OPTIMIZER},
+                'LOSS':            {'value': args.LOSS},
+                'IMG_WIDTH':       {'value': args.IMG_WIDTH},
+                'IMG_HEIGHT':      {'value': args.IMG_HEIGHT},
+                'DROPOUT':         {'value': args.DROPOUT},
+                'WEIGHT_DECAY':    {'value': args.WEIGHT_DECAY},
+                'VALIDATION_SAMPLES': {'value': args.VALIDATION_SAMPLES},
+                'data_augmentation_HF': {'values': [True, False]},
+                'data_augmentation_R': {'values': [0, 20]},#{'max': 20, 'min': 0, 'type': 'int'},
+                'data_augmentation_Z': {'values': [0, 0.2]},#{'max': 0.20, 'min': 0.0, 'type': 'double'},
+                'data_augmentation_W': {'values': [0, 0.2]},#{'max': 0.20, 'min': 0.0, 'type': 'double'},
+                'data_augmentation_H': {'values': [0, 0.2]},#{'max': 0.20, 'min': 0.0, 'type': 'double'},
+                'data_augmentation_S': {'values': [0, 0.2]} #{'max': 0.20, 'min': 0.0, 'type': 'double'}
+            }   
+        }
+
+    sweep_id = wandb.sweep(sweep = sweep_config, project="M3_W4")
+    """
 
 wandb.agent(sweep_id, function=train, count=37)
